6ce5307b07f6e9a676d0e92a7d13f92618861376
aurelo <zlatko.gudasic@gmail.com>
2023-09-14 15:36:23 +0200

    added trx_pkg package body

---
+++ TRX_PKG_body.sql
@@ -0,0 +1,121 @@
+create or replace package body trx_pkg
+as
+-------------------------------------------------------------------------------
+-- PRIVATE PROCEDURES AND FUNCTIONS
+-------------------------------------------------------------------------------
+  function new_transaction
+  return   trxs.id%type
+  is
+    v_trx_id   trxs.id%type;
+  begin
+    insert into trxs (
+     id
+    ,created_at
+    )
+    values (
+     trxs_seq.nextval
+    ,sysdate
+    )
+    returning id
+    into v_trx_id
+    ;
+    
+    return v_trx_id;
+  end;
+-------------------------------------------------------------------------------
+-- PUBLIC PROCEDURES AND FUNCTIONS
+-------------------------------------------------------------------------------
+  procedure transfer(
+    p_to       in     varchar2  default null
+   ,p_amount   in     number    default 0
+  )
+  is
+     cursor cur_credit_acc
+     is
+     select   *
+     from     accounts a
+     where    upper(a.owner) = upper(user)
+     ;
+     v_credit_acc_rec    accounts%rowtype;
+     
+     cursor cur_debit_acc
+     is
+     select   *
+     from     accounts a
+     where    upper(a.owner) = upper(p_to)
+     ;
+      
+     v_debit_acc_rec     accounts%rowtype;
+     
+     v_trx_id            trxs.id%type;
+  begin
+     dbms_output.put_line('Current user: '||user||' pays to: '||p_to||' amount: '||p_amount);
+  
+     open cur_credit_acc;
+     
+     fetch cur_credit_acc
+     into  v_credit_acc_rec;
+     
+     close cur_credit_acc;
+     
+     
+     if p_amount > v_credit_acc_rec.balance 
+     then
+        RAISE_APPLICATION_ERROR(-20000, 'Not enough balance! Required '||p_amount||' available: '||v_credit_acc_rec.balance);
+     elsif p_amount <= 0
+     then
+        RAISE_APPLICATION_ERROR(-20000, 'Amount: '||p_amount||' is not greater then zero!');
+     end if;
+  
+     dbms_output.put_line('Current user: '||user||' with account id: '||v_credit_acc_rec.id||' amount: '||v_credit_acc_rec.balance);
+     
+     open cur_debit_acc;
+     
+     fetch cur_debit_acc
+     into  v_debit_acc_rec;
+     
+     close cur_debit_acc;
+     
+     v_trx_id := new_transaction;
+     
+     insert into trx_items (
+              id
+            , trx_id    
+            , acc_id    
+            , type      
+            , amount    
+            )
+            values (
+              trx_line_seq.nextval
+            ,v_trx_id
+            ,v_credit_acc_rec.id
+            ,'C'
+            , p_amount
+            );
+     
+     update accounts 
+     set    balance = balance - p_amount
+     where  id = v_credit_acc_rec.id
+     ;
+            
+     insert into trx_items (
+              id
+            , trx_id    
+            , acc_id    
+            , type      
+            , amount    
+            )
+            values (
+              trx_line_seq.nextval
+            ,v_trx_id
+            ,v_debit_acc_rec.id
+            ,'D'
+            , p_amount
+            );
+            
+     update accounts 
+     set    balance = balance + p_amount
+     where  id = v_debit_acc_rec.id
+     ;
+  end;
+end;
